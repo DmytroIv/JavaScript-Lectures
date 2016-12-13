b2b867a49d628482afbcfc61d11c9251e71e30b8
Igor Vasilevich <igor.vasilevi4@gmail.com>
2016-12-13 21:19:46 +0200

    introducing to javaScript types variables, objects, functions

---
+++ js/main.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// alert('Hello world!!!');
+
+// var a = prompt('Write great text','');
+// console.log(a);
+
+// debugger;
+
+// var a; simple variable
+// var MY_CONSTANT = "text"; constant
+
+// x = 3; error in strict mode when create global variable
+// var x = 5;
+
+// var x = 1;
+// var y = "text";
+// var b = true;
+// var z = null;
+// var o = {};
+// var a = [];
+// var f = function(){};
+// var u = undefined;
+// var regExp = /www/i;
+// var notAnumb = NaN;
+
+
+// console.log(typeof x, '= numder');
+// console.log(typeof y, '= string');
+// console.log(typeof b, '= boolean');
+// console.log(typeof z, 'null has typeof object');
+// console.log(typeof o, ' = object');
+// console.log(typeof a, 'arr = object');
+// console.log(typeof f, 'func = function');
+// console.log(typeof u, 'undefined');
+// console.log(typeof regExp, 'regularExpresion = object');
+// console.log(typeof notAnumb, 'Nan = number');
+
+// var x = [];
+// console.log(Array.isArray(x)); //true
+
+// var y = {};
+// console.log(Array.isArray(y)); //false
+
+// var x = function (){};
+// x.key1 = 'test'; //bad practic
+// console.log(x.key1);
+
+// var x = ['asdasd' ,1, 'asdad', 123, 'adads'];
+// x.key1 = '123';
+// console.log(x.key1);
+// console.log(x);
+
+
+
