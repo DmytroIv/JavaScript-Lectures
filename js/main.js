3285eb1679e27e96b067d5d0ff7a9efe046bfaf2
Dmytro <dmytropopovkh@gmail.com>
2017-02-04 09:23:24 +0200

    page 7 without last exercise

--- js/main.js
+++ js/main.js
@@ -89,7 +89,6 @@
 		getPayDayDate: getPayDayDate
 	});
 	//Functions
-		
 	function getDayByDate(year, month, day) {
 		var weekDay = new Date(year, month, day),
         daysOfWeekArray = ["su", "mo", "tu", "we", "th", "fr", "sa"];
@@ -159,7 +158,7 @@
 	// console.log(getPayDayDate(2017, 1));
 
 })();
-
+//work at home
 (function (){
     api.addModule("commonServices",{
         getTypeOf: getTypeOf,
@@ -201,12 +200,73 @@
     // console.log(combine({"a": 1, "ab": 55, "ac": "was"}, {"b": true, "bb": [1,2,3], "bc": 11}));
     // console.log(combine(/asd/, null));
 
-    console.log(limitTo("12345abcde", 2));
-    console.log(limitTo([1, 2, 3, 4, 5], 2));
-    console.log(limitTo(123, 122));
-    console.log(limitTo({"a": 1, "ab": 55, "ac": "was"}));
-    console.log(limitTo(/asd/, null));
+    // console.log(limitTo("12345abcde", 2));
+    // console.log(limitTo([1, 2, 3, 4, 5], 2));
+    // console.log(limitTo(123, 122));
+    // console.log(limitTo({"a": 1, "ab": 55, "ac": "was"}));
+    // console.log(limitTo(/asd/, null));
+})();
 
+(function (){
+    api.addModule("arrayServices", {
+        filterByField: filterByField,
+        findByField: findByField,
+        sortByField: sortByField
+    });
+    //Functions
+	function filterByField(array, field) {
+		return array.filter(function(element, i, array) {
+            var currentEl = element[field];
+           return !(currentEl === false || currentEl === null || currentEl === undefined);
+		});
+	}
+
+    function findByField(array, field, value) {
+		var obj = undefined;
+		array.forEach(function(element, i, array){
+			for(var key in array[i]){
+				if(key === field && array[i][key] === value){
+					obj = {};
+				 	obj[key] = value;
+				}
+			}
+		});
+		return obj;
+	}
+    function sortByField(array, field, direction) {}
+
+    /*var arr = [
+        {1:1, first: false, second: undefined},
+        {2:2, first: -1, second: "asd"},
+        {3:3, first: 0, second: 123},
+        {4:4, first: 3,second: "dsa" },
+        {5:5, first: 12.2, second: null },
+        {6:6, first: "", second: [1,2,3] },
+        {7:7, first: null, second: Infinity},
+        {8:8, first: null, second: Infinity},
+        {9:9, first: null, second: Infinity},
+        {10:10, first: NaN, second: null},
+        {11:11, first: undefined, second: undefined},
+        {12:12, first: undefined, second: undefined},
+        {13:13, first: undefined, second: null},
+        {14:14, first: undefined, second: null}
+    ];
+	arr = filterByField(arr, "second");*/
+
+	/*var arrFields = [{"asd": 1, 2: "qwe", "ads":"zxc"},
+        {55: 1, "er": "qwe", 3:"123"},
+        {"mkl": "lpl", "ddf2": "mk", "a":"z"},
+        {"mkl": "lpl", "ddf2": "mk", "a":"z"}
+	];
+    console.log(findByField(arrFields, "55", 1));*/
+
+})();
+
+(function(){
+    api.addModule("storagesServices", {
+
+	});
+    //Functions
 
 })();
 
@@ -225,6 +285,79 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getTypeOf(elem){
+    return {}.toString.call(elem).slice(8, -1);
+}
+
+function getSortedArray(arrayOfObjects, field, order) {
+            return sortingArray(arrayOfObjects, field, order);
+}
+
+function sortingArray(array, sortBy, order){
+    return array.sort(function (a, b) {
+        if (getTypeOf(a) === "Object" && a !== null){
+            if ((typeof a[sortBy] === "string" && typeof b[sortBy] === "string") && isNaN(parseInt(a[sortBy]))) {
+                var a = a[sortBy].toLowerCase(),
+                    b = b[sortBy].toLowerCase();
+                return order === "descending" ? (a < b ? 1 : a > b ? -1 : 0) : (a > b ? 1 : a < b ? -1 : 0);
+            }
+            return order === "descending" ? b[sortBy] - a[sortBy] : a[sortBy] - b[sortBy];
+        }
+        if(getTypeOf(a) === "Date"){      console.log("ooops!");        return a.getTime() - b.getTime();}
+        return (typeof a  === "string" && typeof b === "string") ? a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0) : a - b ;
+    });
+}
+
+var personFrom = [
+    {"name": "Zasya","age": "111"},
+    {"name": "asdsya","age": "12"},
+    {"name": 123,"age": "111"},
+    {"name": "Dusia","age": "33"},
+    {"name":  new Date(2017, 2, 35),"age": 10},
+    {"name":  new Date(2011, 7, 25), "age": "13"},
+    {"name": "Zztia","age": 15},
+    {"name": "Gadia","age": "22"},
+    {"name": new Date(2014, 2, 15),"age": 30},
+    {"name": "Misha","age": 25}
+];
+
+
+//console.log(getSortedArray(personFrom, "name"));
 
 
 
