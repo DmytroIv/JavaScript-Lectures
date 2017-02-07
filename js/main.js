ae433760654eea3b4b13364b583073373f30a7a2
DmytroIv <dmytropopovkh@gmail.com>
2017-02-07 21:53:58 +0200

    jQuery

---
+++ js/main.js
@@ -0,0 +1,91 @@
+//Awaiting document loading;
+// $(document).ready(function(){
+	// console.log("ready 1");
+// });
+
+// $(document).on("ready", function(){
+	// console.log("ready 2");
+// });
+
+// $(function () {
+	// console.log("ready 3");
+// });
+
+//Navigation
+var mainTitle = $("h1.mainTitle");
+var spanText = $("span.text");
+
+// console.log(mainTitle.children());
+// console.log(mainTitle.children("input"));
+// console.log(mainTitle.parent());
+// console.log(mainTitle.parent("div"));
+// console.log(mainTitle.parents("body"));
+// console.log(mainTitle.parents());
+// console.log(mainTitle.find("*"));
+// console.log(mainTitle.find(".text"));
+
+// console.log(spanText.prev());
+// console.log(spanText.prev("div"));
+// console.log(spanText.next());
+// console.log(spanText.next("input"));
+
+// mainTitle.addClass("test");
+// console.log(mainTitle.addClass("test"));
+// mainTitle.removeClass("test");
+// console.log(mainTitle.removeClass("test"));
+// mainTitle.addClass("test");
+
+// console.log($("img").attr("src"));
+// $("img").attr("src", "images/test.jpg");
+// $("img").removeAttr("src");
+
+// console.log($("img").prop("src"));
+// $("img").prop("src", "asd");
+// $("img").removeProp("src");
+// console.log($("img")[0]);
+// console.log(mainTitle[0]);
+
+// console.log(mainTitle.html());
+// console.log(mainTitle[0].innerHTML);
+// mainTitle.html("<div>text</div>");
+// mainTitle[0].innerHTML = "<div>text</div>";
+
+// var clickableButton = $("<div>clickable button</div>")
+	// .css("color", "red")
+	// .on("click", function() {alert("asd")});
+// mainTitle.find("span.text").html(clickableButton);
+
+console.log($("input").val());
+$("input").val("test")
+console.log($("input").val());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
