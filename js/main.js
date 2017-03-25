53fb64d72a954c6cae81bdddaf155dff37acf4f0
DmytroIv <dmytropopovkh@gmail.com>
2017-03-25 13:45:47 +0200

    Lecture 23 angular

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 	"use strict";
 	angular.module("app", [
 		"price",
-		"toHtml"
+		"toHtml",
+		"myComponent"
 	]).run(function($templateCache){
 		$templateCache.put("myTemplate.html", "this is the <br/> content of the template");
 	});
@@ -47,14 +48,39 @@
 		myCtrl.html = /*$sce.trustAsHtml(*/"<h1>test test</h1>";//);
 		myCtrl.html2 = /*$sce.trustAsHtml(*/$templateCache.get("myTemplate.html");
 		myCtrl.timer = new Date().getSeconds();
-			
+		myCtrl.date = $.now();
+		myCtrl.usersFilter = "";
+		myCtrl.names = [];
+		myCtrl.selectedOption = "";
+		myCtrl.imageSrc = "";
+
+		myCtrl.options = [
+			{value: "", label: "select item"},
+			{value: "Item1", label: "item1 label"},
+			{value: "Item2", label: "item2 label"},
+			{value: "Item3", label: "item3 label"},
+			{value: "Item4", label: "item4 label"},
+			{value: "Item5", label: "item5 label"}
+		];
+
+		myCtrl.onBlur = onBlur;
 		myCtrl.askUser = askUser;
+		myCtrl.usersFilterMethod = usersFilterMethod;
+
+		function onBlur($event){
+			console.log($event);
+		}
+
+		function usersFilterMethod(elem, index, array){
+			// console.log(elem, index, array);
+			return ~elem.name.toLowerCase().indexOf(myCtrl.usersFilter.toLowerCase());
+		}
 		
 		$scope.$applyAsync(function(){
 			myCtrl.serverData = {"myKey1": "asdasd"};
 		});
 	
-		console.log($scope);
+		// console.log($scope);
 	
 		$http({"method": "GET", "url":"http://www.mocky.io/v2/58d4161a100000db0cd7a6b5"}).then(function(data){
 			myCtrl.serverData = data.data;
@@ -64,6 +90,7 @@
 		function askUser(){
 			myModal().then(function(userAnswer){
 				myCtrl.userAction = userAnswer;
+				myCtrl.imageSrc = "images/logo.png";
 			});
 		}
 		
@@ -81,3 +108,41 @@
 		}, 1000, 5, false, new Date().getSeconds());
 	}
 })();
+
+(function (){
+	"use strict";
+	angular.module("myComponent", []).component("myComponent", myComponent());
+
+	function myComponent(){
+		return {
+			template: "<div><h1>Component title</h1><ng-transclude></div>",
+			transclude: true
+		};
+	}
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
